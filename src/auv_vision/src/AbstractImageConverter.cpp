ce44a1ec3e9945ce1aa93d68a7f0adbf7bb28f8f
TimeEscaper <rentgeny05@gmail.com>
2018-10-14 20:34:46 +0300

    Base class for working with cv_bridge added; gate_locator node refactored

---
+++ src/auv_vision/src/AbstractImageConverter.cpp
@@ -0,0 +1,24 @@
+#include "AbstractImageConverter.h"
+
+AbstractImageConverter::AbstractImageConverter()
+    : imageTransport(nodeHandle)
+{
+    imageSubscriber = imageTransport.subscribe(inputImageTopic, 1, &AbstractImageConverter::imageCb, this);
+}
+
+void AbstractImageConverter::imageCb(const sensor_msgs::ImageConstPtr &msg)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    process(cv_ptr);
+}
+
