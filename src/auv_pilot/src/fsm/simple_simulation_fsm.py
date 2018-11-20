7e0399a1210686a970db0b4f411439ed18d432d9
TimeEscaper <rentgeny05@gmail.com>
2018-11-20 06:51:29 +0300

    Simple basic fsm implemented

--- src/auv_pilot/src/fsm/simple_simulation_fsm.py
+++ src/auv_pilot/src/fsm/simple_simulation_fsm.py
@@ -3,9 +3,16 @@
 import rospy
 import smach
 import smach_ros
+import actionlib
+import actionlib_msgs
 from auv_common.msg import OptionalPoint2D
+from auv_common.msg import MoveByTimeActionGoal, MoveByTimeAction
 
+def exploreGate(userData, gateMessage):
+    return not gateMessage.hasPoint
 
+def centerGate(userData, gateMessage):
+    return not (gateMessage.hasPoint and abs(gateMessage.x) < 5)
 
 def main():
     rospy.init_node('simple_simulation_fsm')
@@ -13,12 +20,41 @@ def main():
     sm = smach.StateMachine(outcomes=['SUCCESS', 'ABORTED'])
 
     with sm:
+
         smach.StateMachine.add('EXPLORE', 
                                 smach_ros.MonitorState(
                                     '/gate',
                                     OptionalPoint2D,
-                                    {'valid':'EXPLORE', 'invalid':'SUCCESS'}
-                                ))
+                                    exploreGate),
+                                {'valid':'EXPLORE', 'invalid':'SIDE_MOVE', 'preempted':'ABORTED'})
+
+        sideMoveGoal = MoveByTimeActionGoal()
+        sideMoveGoal.goal.direction = 2
+        sideMoveGoal.goal.time = 0
+        smach.StateMachine.add('SIDE_MOVE',
+                                smach_ros.SimpleActionState(
+                                    'auv_pilot',
+                                    MoveByTimeAction,
+                                    goal=sideMoveGoal),
+                                {'succeeded':'GATE_MONITOR', 'preempted':'ABORTED', 'aborted':'ABORTED'})
+
+        smach.StateMachine.add('GATE_MONITOR', 
+                                smach_ros.MonitorState(
+                                    '/gate',
+                                    OptionalPoint2D,
+                                    centerGate),
+                                {'valid':'GATE_MONITOR', 'invalid':'FORWARD_MOVE', 'preempted':'ABORTED'})
+
+        forwardMoveGoal = MoveByTimeActionGoal()
+        forwardMoveGoal.goal.direction = 1
+        forwardMoveGoal.goal.direction = 0
+        smach.StateMachine.add('FORWARD_MOVE',
+                                smach_ros.SimpleActionState(
+                                    'auv_pilot',
+                                    MoveByTimeAction,
+                                    goal=forwardMoveGoal),
+                                {'succeeded':'SUCCESS', 'preempted':'ABORTED', 'aborted':'ABORTED'})
+                                
     
     outcome = sm.execute()
 
