048365ae8db7364019c8f7cfde539de8b52d6c15
TimeEscaper <rentgeny05@gmail.com>
2018-12-17 11:08:56 +0300

    Rotate action moved to Move action

--- src/auv_pilot/src/MoveActionServerBase.cpp
+++ src/auv_pilot/src/MoveActionServerBase.cpp
@@ -45,13 +45,35 @@ geometry_msgs::Twist MoveActionServerBase::createRotationTwist(int direction, fl
 geometry_msgs::Twist MoveActionServerBase::createDirectionTwist(int direction, float velocity) {
     switch (direction) {
         case auv_common::MoveGoal::DIRECTION_FORWARD:
-            return createLinearTwist(velocity, 0.0f, 0.0f); // Workaraound for current model
+            return createLinearTwist(velocity, 0.0f, 0.0f);
+
         case auv_common::MoveGoal::DIRECTION_BACKWARDS:
             return createLinearTwist(-velocity, 0.0f, 0.0f);
+
         case auv_common::MoveGoal::DIRECTION_RIGHT:
             return createLinearTwist(0, velocity, 0.0f);
+
         case auv_common::MoveGoal::DIRECTION_LEFT:
             return createLinearTwist(0, -velocity, 0.0f);
+
+        case auv_common::MoveGoal::ROTATE_ROLL_CW:
+            return createAngularTwist(velocity, 0.0f, 0.0f);
+
+        case auv_common::MoveGoal::ROTATE_ROLL_CCW:
+            return createAngularTwist(-velocity, 0.0f, 0.0f);
+
+        case auv_common::MoveGoal::ROTATE_PITCH_CW:
+            return createAngularTwist(0.0f, velocity, 0.0f);
+
+        case auv_common::MoveGoal::ROTATE_PITCH_CCW:
+            return createAngularTwist(0.0f, -velocity, 0.0f);
+
+        case auv_common::MoveGoal::ROTATE_YAW_CW:
+            return createAngularTwist(0.0f, 0.0f, velocity);
+
+        case auv_common::MoveGoal::ROTATE_YAW_CCW:
+            return createAngularTwist(0.0f, 0.0f, -velocity);
+
         default:
             return createLinearTwist(0.0f, 0.0f, 0.0f);
     }
