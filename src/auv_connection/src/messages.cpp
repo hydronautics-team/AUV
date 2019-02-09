8a43e51e9b01bf525f7a2b58bd78359a51ed5d4d
YKamenev322 <realmanlive@gmail.com>
2019-02-09 17:55:05 +0300

    Checksum fixed

--- src/auv_connection/src/messages.cpp
+++ src/auv_connection/src/messages.cpp
@@ -407,15 +407,15 @@ void popFromVector(std::vector<uint8_t> &vector, float &output, bool revert)
   */
 uint16_t getChecksum16b(std::vector<uint8_t> &vector)
 {
-    uint8_t *pcBlock = &vector[0]; 
     uint16_t len = vector.size();
     uint16_t crc = 0xFFFF;
     //int crc_fix = reinterpret_cast<int*>(&crc);
     uint8_t i;
-    len = len-2;
+    uint8_t g = 0;
 
     while (len--) {
-        crc ^= *pcBlock++ << 8;
+        crc ^= vector[g] << 8;
+        g++;
 
         for (i = 0; i < 8; i++)
             crc = crc & 0x8000 ? (crc << 1) ^ 0x1021 : crc << 1;
