7f72a2057f965d8fae7b638de003b8321b76b41a
TimeEscaper <rentgeny05@gmail.com>
2019-02-11 13:24:51 +0300

    OpenCV windows enablement arugment added

--- src/auv_vision/src/gate_locator.cpp
+++ src/auv_vision/src/gate_locator.cpp
@@ -9,14 +9,15 @@
 
 static const std::string OPENCV_WINDOW = "Image window";
 
+static const std::string ENABLE_WINDOWS_PARAM = "debugVision";
+
 static const std::string CAMERA_TOPIC = "/cam_front_1/image_raw";
 
 static const std::string GATE_PUBLISH_TOPIC = "/gate";
 
 static const std::string GATE_LOCATOR_NODE_NAME = "gate_locator";
 
-class GatePublisher : public AbstractImageConverter
-{
+class GatePublisher : public AbstractImageConverter {
 
 private:
 
@@ -24,55 +25,56 @@ private:
 
     GateDetector detector;
 
+    bool windowsEnabled;
+
 protected:
 
-    void process(const cv_bridge::CvImagePtr& cv_ptr)
-    {
-      cv::Mat image = cv_ptr->image;
+    void process(const cv_bridge::CvImagePtr &cv_ptr) {
+        cv::Mat image = cv_ptr->image;
 
-      GateDescriptor gate = detector.detect(image, true);
+        GateDescriptor gate = detector.detect(image, true);
 
-      auv_common::OptionalPoint2D msg;
+        auv_common::OptionalPoint2D msg;
 
-      if (gate.hasGate()) {
-          cv::Point2f center = convertToCentralCoordinates(gate.getCenter(), image.cols, image.rows);
+        if (gate.hasGate()) {
+            cv::Point2f center = convertToCentralCoordinates(gate.getCenter(), image.cols, image.rows);
 
-          msg.hasPoint = true;
-          msg.x = center.x;
-          msg.y = center.y;
-          gatePublisher.publish(msg);
+            msg.hasPoint = true;
+            msg.x = center.x;
+            msg.y = center.y;
+            gatePublisher.publish(msg);
 
-          std::vector<cv::Point2f> corners = gate.getCorners();
-          cv::circle(image, corners[0], 10, CV_RGB(0,255,0));
-          cv::circle(image, corners[1], 10, CV_RGB(0,255,0));
-          cv::circle(image, corners[2], 10, CV_RGB(0,255,0));
-          cv::circle(image, corners[3], 10, CV_RGB(0,255,0));
-          cv::circle(image, gate.getCenter(), 10, CV_RGB(0,0,255));
-      } else {
-          msg.hasPoint = false;
-          msg.x = 0;
-          msg.y = 0;
-          gatePublisher.publish(msg);
-      }
+            std::vector<cv::Point2f> corners = gate.getCorners();
+            cv::circle(image, corners[0], 10, CV_RGB(0, 255, 0));
+            cv::circle(image, corners[1], 10, CV_RGB(0, 255, 0));
+            cv::circle(image, corners[2], 10, CV_RGB(0, 255, 0));
+            cv::circle(image, corners[3], 10, CV_RGB(0, 255, 0));
+            cv::circle(image, gate.getCenter(), 10, CV_RGB(0, 0, 255));
+        } else {
+            msg.hasPoint = false;
+            msg.x = 0;
+            msg.y = 0;
+            gatePublisher.publish(msg);
+        }
 
-
-      cv::imshow(OPENCV_WINDOW, image);
-      cv::waitKey(3);
-
+        if (windowsEnabled) {
+            cv::imshow(OPENCV_WINDOW, image);
+            cv::waitKey(3);
+        }
     }
 
 public:
 
-    GatePublisher(const std::string& inputImageTopic) : AbstractImageConverter(inputImageTopic)
-    {
-      gatePublisher = nodeHandle.advertise<auv_common::OptionalPoint2D>(GATE_PUBLISH_TOPIC, 100);
-
-      cv::namedWindow(OPENCV_WINDOW, CV_WINDOW_AUTOSIZE);
+    GatePublisher(const std::string &inputImageTopic, bool enableWindows) : AbstractImageConverter(inputImageTopic),
+                                                                            windowsEnabled(enableWindows) {
+        gatePublisher = nodeHandle.advertise<auv_common::OptionalPoint2D>(GATE_PUBLISH_TOPIC, 100);
+        if (windowsEnabled)
+            cv::namedWindow(OPENCV_WINDOW, CV_WINDOW_AUTOSIZE);
     }
 
-    ~GatePublisher()
-    {
-      cv::destroyWindow(OPENCV_WINDOW);
+    ~GatePublisher() {
+        if (windowsEnabled)
+            cv::destroyWindow(OPENCV_WINDOW);
     }
 
 };
@@ -88,10 +90,15 @@ int main(int argc, char **argv)
     ROS_INFO("%s", "OpenCV version = 3");
 #endif
 
-  ros::init(argc, argv, GATE_LOCATOR_NODE_NAME);
-  GatePublisher gatePublisher(CAMERA_TOPIC);
+    ros::init(argc, argv, GATE_LOCATOR_NODE_NAME);
 
-  ros::spin();
+    ros::NodeHandle nodeHandle(GATE_LOCATOR_NODE_NAME);
+    bool windowsEnabled;
+    nodeHandle.param(ENABLE_WINDOWS_PARAM, windowsEnabled, false);
 
-  return 0;
+    GatePublisher gatePublisher(CAMERA_TOPIC, windowsEnabled);
+
+    ros::spin();
+
+    return 0;
 }
