bab4553f10ea170bf7f711139b6a446071391154
TimeEscaper <rentgeny05@gmail.com>
2019-02-26 00:20:41 +0300

    Forward move state added

--- src/auv_pilot/scripts/qualification/qualification_fsm.py
+++ src/auv_pilot/scripts/qualification/qualification_fsm.py
@@ -4,6 +4,7 @@ import rospy
 import smach
 import smach_ros
 from common import common_states
+from auv_common.msg import MoveGoal, MoveAction
 
 
 def create_qualification_fsm(launch_delay, dive_delay, initial_depth):
@@ -11,9 +12,23 @@ def create_qualification_fsm(launch_delay, dive_delay, initial_depth):
     sm = smach.StateMachine(outcomes=['QUALIFICATION_OK', 'QUALIFICATION_FAILED'])
 
     with sm:
+
+        forwardMoveGoal = MoveGoal()
+        forwardMoveGoal.direction = MoveGoal.DIRECTION_FORWARD
+        forwardMoveGoal.value = 10000 # 10,000 msec = 10 sec
+        forwardMoveGoal.velocityLevel = MoveGoal.VELOCITY_LEVEL_4 # Maximal velocity
+        forwardMoveGoal.holdIfInfinityValue = False
+
+
         smach.StateMachine.add('LAUNCH_DELAY', common_states.WaitState(launch_delay), transitions={'OK': 'DIVE_DELAY'})
         smach.StateMachine.add('DIVE_DELAY', common_states.WaitState(dive_delay), transitions={'OK': 'DIVE'})
         smach.StateMachine.add('DIVE', common_states.create_diving_state(initial_depth), transitions={
-            'succeeded':'QUALIFICATION_OK', 'preempted':'QUALIFICATION_FAILED', 'aborted':'QUALIFICATION_FAILED'})
-        # TODO: Add forward move
+            'succeeded':'FORWARD_MOVE', 'preempted':'QUALIFICATION_FAILED', 'aborted':'QUALIFICATION_FAILED'})
+        smach.StateMachine.add('FORWARD_MOVE',
+                                smach_ros.SimpleActionState(
+                                    'move_by_time',
+                                    MoveAction,
+                                    goal=forwardMoveGoal),
+                                {'succeeded':'QUALIFICATION_OK', 'preempted':'QUALIFICATION_FAILED', 'aborted':'QUALIFICATION_FAILED'})
+
     return sm
