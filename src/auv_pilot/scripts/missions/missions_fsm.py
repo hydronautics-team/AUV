4aaeb9742231a0a9c6094513824d382c4263119c
TimeEscaper <rentgeny05@gmail.com>
2019-02-28 18:17:47 +0300

    Gate mission fsm updated

--- src/auv_pilot/scripts/missions/missions_fsm.py
+++ src/auv_pilot/scripts/missions/missions_fsm.py
@@ -16,10 +16,10 @@ def create_missions_fsm(launch_delay, dive_delay, initial_depth):
         smach.StateMachine.add('LAUNCH_DELAY', common_states.WaitState(launch_delay), transitions={'OK': 'DIVE_DELAY'})
         smach.StateMachine.add('DIVE_DELAY', common_states.WaitState(dive_delay), transitions={'OK': 'DIVE'})
         smach.StateMachine.add('DIVE', common_states.create_diving_state(initial_depth), transitions={
-            'succeeded':'DRUMS_MISSION', 'preempted':'MISSIONS_FAILED', 'aborted':'MISSIONS_FAILED'})
-        #smach.StateMachine.add('GATE_MISSION', gate_mission.create_gate_fsm(), transitions={'GATE_OK': 'SUCCESS', 'GATE_FAILED': 'ABORTED'})
+            'succeeded':'GATE_MISSION', 'preempted':'MISSIONS_FAILED', 'aborted':'MISSIONS_FAILED'})
+        smach.StateMachine.add('GATE_MISSION', gate_mission.create_gate_fsm(), transitions={'GATE_OK': 'MISSIONS_OK', 'GATE_FAILED': 'MISSIONS_FAILED'})
         # TODO THIS IS ONLY FOR DEBUGGING
-        smach.StateMachine.add('DRUMS_MISSION', drums_mission.create_drums_fsm(), transitions={'DRUMS_OK': 'MISSIONS_OK', 'DRUMS_FAILED': 'MISSIONS_FAILED'})
+        #smach.StateMachine.add('DRUMS_MISSION', drums_mission.create_drums_fsm(), transitions={'DRUMS_OK': 'MISSIONS_OK', 'DRUMS_FAILED': 'MISSIONS_FAILED'})
 
     return sm
 
