be21e62248d0ec3ed539a625a0c2e53c2a4bc600
TimeEscaper <rentgeny05@gmail.com>
2019-03-07 22:19:18 +0300

    QR triggering refactored

--- src/auv_pilot/scripts/top_level_fsm.py
+++ src/auv_pilot/scripts/top_level_fsm.py
@@ -3,111 +3,80 @@
 import rospy
 import smach
 import smach_ros
-import auv_common.srv
+import std_srvs.srv
 from common import common_states
 from qualification import qualification_fsm
 from missions import missions_fsm
 from demo import demo_fsm
 
-launched = False
-receivedMode = None
-
-def launch_callback(req):
-    global launched
-    global receivedMode
-    mode = req.mode.upper()
-    if mode not in ['QUALIFICATION_SIMPLE', 'QUALIFICATION_VISION', 'MISSIONS', 'DEMO']:
-        return auv_common.srv.LaunchCmdResponse(False,
-                                                'No executable mode specified. Allowed executable modes: qualification_simple, qualification_vision, missions, demo.')
-    if launched:
-        return auv_common.srv.LaunchCmdResponse(False, 'Already launched')
-    receivedMode = mode
-    launched = True
-    return auv_common.srv.LaunchCmdResponse(True, 'OK')
-
 
 def main():
-    global receivedMode
-    global launched
-
     rospy.init_node('top_level_fsm')
 
-    startCondition = rospy.get_param('~startCondition', 'none').upper()
-    if startCondition not in ['AUTO', 'TRIGGER']:
-        rospy.logerr('Unknown start condition. Available start conditions: auto, trigger')
+    mode = rospy.get_param('~mode', 'none').upper()
+    rospy.loginfo(mode)
+    if mode not in ['QUALIFICATION_SIMPLE', 'QUALIFICATION_VISION', 'MISSIONS', 'DEMO']:
+        rospy.loginfo('No executable mode specified. Allowed executable modes: qualification_simple, qualification_vision, missions, demo.')
+        return
+    rospy.loginfo("FSM mode: " + mode)
 
-    if startCondition == 'TRIGGER':
-        rospy.loginfo('Waiting for launch trigger')
-        fsm_start_service = rospy.Service('fsm_start', auv_common.srv.LaunchCmd, launch_callback)
-    else:
-        launched = True
+    if mode != 'DEMO':
+        if not (rospy.has_param('~diveDelay') or rospy.has_param('~initialDepth') or
+                rospy.has_param('~imuReset')):
+            rospy.logerr("dive_delay, initial_depth, imu_reset must be set!")
+            raise
+        dive_delay = int(rospy.get_param('~diveDelay'))
+        initial_depth = int(rospy.get_param('~initialDepth'))
+        imu_reset = bool(rospy.get_param('~imuReset'))
+        rospy.loginfo("Dive delay: " + str(dive_delay) + " Initial depth: " + str(initial_depth) + " IMU reset: " + str(imu_reset))
+    if mode == 'QUALIFICATION_SIMPLE':
+        if not (rospy.has_param('~qualificationDuration')):
+            rospy.logerr("qualification_duration parameter must be set for qualification simple mode")
+            raise
+        qualification_duration = int(rospy.get_param('~qualificationDuration'))
+        rospy.loginfo("Qualification forward move duration: " + str(qualification_duration))
 
-    while not rospy.is_shutdown():
+    notifyOnFinish = rospy.get_param('~notifyOnFinish', False)
 
-        if launched:
+    sm = smach.StateMachine(outcomes=['SUCCEEDED', 'FAILED'])
 
-            if startCondition == 'AUTO':
-                mode = rospy.get_param('~mode', 'none').upper()
-            else:
-                mode = receivedMode
+    with sm:
 
-            rospy.loginfo(mode)
-            if mode not in ['QUALIFICATION_SIMPLE', 'QUALIFICATION_VISION', 'MISSIONS', 'DEMO']:
-                rospy.loginfo('No executable mode specified. Allowed executable modes: qualification_simple, qualification_vision, missions, demo.')
-                return
-            rospy.loginfo("FSM mode: " + mode)
+        if mode != 'DEMO':
+            if imu_reset:
+                smach.StateMachine.add('IMU_INIT', common_states.IMUInitState(), transitions={'OK': 'DIVE_DELAY'})
+            smach.StateMachine.add('DIVE_DELAY', common_states.WaitState(dive_delay), transitions={'OK': 'STABILIZATION_INIT'})
+            smach.StateMachine.add('STABILIZATION_INIT', common_states.StabilizationInitState(), transitions={'OK': 'DIVE'})
+            smach.StateMachine.add('DIVE', common_states.create_diving_state(initial_depth),
+                                   transitions={'succeeded':mode, 'preempted':'SUCCEEDED', 'aborted':'FAILED'})
 
-            if mode != 'DEMO':
-                if not (rospy.has_param('~diveDelay') or rospy.has_param('~initialDepth') or
-                        rospy.has_param('~imuReset')):
-                    rospy.logerr("dive_delay, initial_depth, imu_reset must be set!")
-                    raise
-                dive_delay = int(rospy.get_param('~diveDelay'))
-                initial_depth = int(rospy.get_param('~initialDepth'))
-                imu_reset = bool(rospy.get_param('~imuReset'))
-                rospy.loginfo("Dive delay: " + str(dive_delay) + " Initial depth: " + str(initial_depth) + " IMU reset: " + str(imu_reset))
-            if mode == 'QUALIFICATION_SIMPLE':
-                if not (rospy.has_param('~qualificationDuration')):
-                    rospy.logerr("qualification_duration parameter must be set for qualification simple mode")
-                    raise
-                qualification_duration = int(rospy.get_param('~qualificationDuration'))
-                rospy.loginfo("Qualification forward move duration: " + str(qualification_duration))
+        if mode == 'QUALIFICATION_SIMPLE':
+            smach.StateMachine.add('QUALIFICATION_SIMPLE', qualification_fsm.create_qualification_simple_fsm(qualification_duration),
+                                   transitions={'QUALIFICATION_OK': 'SUCCEEDED', 'QUALIFICATION_FAILED': 'FAILED'})
 
-            sm = smach.StateMachine(outcomes=['SUCCEEDED', 'FAILED'])
+        elif mode == 'QUALIFICATION_VISION':
+            smach.StateMachine.add('QUALIFICATION_VISION', qualification_fsm.create_qualification_vision_fsm(),
+                                   transitions={'QUALIFICATION_OK': 'SUCCEEDED', 'QUALIFICATION_FAILED': 'FAILED'})
 
-            with sm:
+        elif mode == 'MISSIONS':
+            smach.StateMachine.add('MISSIONS', missions_fsm.create_missions_fsm(),
+                                   transitions={'MISSIONS_OK': 'SUCCEEDED', 'MISSIONS_FAILED': 'FAILED'})
 
-                if mode != 'DEMO':
-                    if imu_reset:
-                        smach.StateMachine.add('IMU_INIT', common_states.IMUInitState(), transitions={'OK': 'DIVE_DELAY'})
-                    smach.StateMachine.add('DIVE_DELAY', common_states.WaitState(dive_delay), transitions={'OK': 'STABILIZATION_INIT'})
-                    smach.StateMachine.add('STABILIZATION_INIT', common_states.StabilizationInitState(), transitions={'OK': 'DIVE'})
-                    smach.StateMachine.add('DIVE', common_states.create_diving_state(initial_depth),
-                                           transitions={'succeeded':mode, 'preempted':'SUCCEEDED', 'aborted':'FAILED'})
+        elif mode == 'DEMO':
+            smach.StateMachine.add('DEMO', demo_fsm.create_demo_fsm(), transitions={'DEMO_OK': 'SUCCEEDED', 'DEMO_FAILED': 'FAILED'})
 
-                if mode == 'QUALIFICATION_SIMPLE':
-                    smach.StateMachine.add('QUALIFICATION_SIMPLE', qualification_fsm.create_qualification_simple_fsm(qualification_duration),
-                        transitions={'QUALIFICATION_OK': 'SUCCEEDED', 'QUALIFICATION_FAILED': 'FAILED'})
+    server = smach_ros.IntrospectionServer('top_level_fsm', sm, '/fsm/top_level_fsm')
+    server.start()
 
-                elif mode == 'QUALIFICATION_VISION':
-                    smach.StateMachine.add('QUALIFICATION_VISION', qualification_fsm.create_qualification_vision_fsm(),
-                                       transitions={'QUALIFICATION_OK': 'SUCCEEDED', 'QUALIFICATION_FAILED': 'FAILED'})
+    outcome = sm.execute()
+    rospy.loginfo('FSM finished')
 
-                elif mode == 'MISSIONS':
-                    smach.StateMachine.add('MISSIONS', missions_fsm.create_missions_fsm(),
-                        transitions={'MISSIONS_OK': 'SUCCEEDED', 'MISSIONS_FAILED': 'FAILED'})
+    if notifyOnFinish:
+        fsm_finished_service = rospy.ServiceProxy('global_fsm_finished', std_srvs.srv.Trigger)
+        fsm_finished_service()
 
-                elif mode == 'DEMO':
-                    smach.StateMachine.add('DEMO', demo_fsm.create_demo_fsm(), transitions={'DEMO_OK': 'SUCCEEDED', 'DEMO_FAILED': 'FAILED'})
-
-            server = smach_ros.IntrospectionServer('top_level_fsm', sm, '/fsm/top_level_fsm')
-            server.start()
-
-            outcome = sm.execute()
-
-            server.stop()
-
-            return
+    rospy.spin()
+    server.stop()
 
 
 if __name__ == '__main__':
--- src/auv_startup/scripts/qr_trigger.py
+++ src/auv_startup/scripts/qr_trigger.py
@@ -2,41 +2,87 @@
 
 import rospy
 import roslaunch
-import os
 import std_msgs.msg
-import auv_common.srv
+import std_srvs.srv
+
 
 launched = False
+requestedMode = None
+launchRequested = False
+stopRequested = False
+
 
 def barcode_callback(msg):
     global launched
+    global launchRequested
+    global stopRequested
+    global requestedMode
 
     message = msg.data.lower()
-    if message not in ['qualification_simple', 'qualification_vision', 'missions', 'shutdown', 'demo']:
+    if message not in ['qualification_simple', 'qualification_vision', 'missions', 'demo', 'stop']:
         rospy.logerr('Unknown messages')
         return
 
-    if message == 'shutdown':
-        rospy.loginfo('Shutting down machine...')
-        os.system('shutdown now')
+    if message == 'stop':
+        if not launched:
+            rospy.logwarn('Not launched yet')
+            return
+        if stopRequested:
+            rospy.logwarn('Stop is already requested')
+            return
+        stopRequested = True
         return
 
     if launched:
         rospy.logwarn('Already launched')
         return
+    if launchRequested:
+        rospy.logwarn('Launch is already requested')
+        return
 
-    launch_service = rospy.ServiceProxy('fsm_start', auv_common.srv.LaunchCmd)
-    resp = launch_service(message)
-    if resp.success:
-        launched = True
+    requestedMode = message
+    launchRequested = True
 
+
+def stop_notification_callback(req):
+    global stopRequested
+    stopRequested = True
+    return std_srvs.srv.TriggerResponse(True, 'OK')
 
 def main():
+    global launched
+    global launchRequested
+    global stopRequested
+    global requestedMode
+
     rospy.init_node('qr_trigger')
 
-    rospy.Subscriber('/barcode', std_msgs.msg.String, barcode_callback)
+    launch = None
 
-    rospy.spin()
+    rospy.Subscriber('/barcode', std_msgs.msg.String, barcode_callback)
+    rospy.Service('global_fsm_finished', std_srvs.srv.Trigger, stop_notification_callback)
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+
+        if launchRequested:
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/sibirsky/factory/AUV/src/auv_startup/launch/" +
+                                                     requestedMode + ".launch"])
+            rospy.loginfo('Starting launch mode ' + requestedMode)
+            launch.start()
+            launched = True
+            launchRequested = False
+        elif stopRequested:
+            rospy.loginfo('Shutting down launch config...')
+            launch.shutdown()
+            launched = False
+            stopRequested = False
+
+        rate.sleep()
+
 
 
 if __name__ == '__main__':
